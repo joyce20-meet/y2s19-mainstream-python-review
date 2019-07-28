28d172eaceabef06f00d04b7649fd250146e2680
myname21-meet <myname21@meet.mit.edu>
2019-07-28 06:45:35 -0400

    did not do anything

--- part1.py
+++ part1.py
@@ -1,13 +1,41 @@
 # Part 1 of the Python Review lab.
 
 def hello_world():
-	pass
+	return("hello world")
 
 def greet_by_name(name):
-	pass
+        return("hello"+ " " + name)
+	
 
 def encode(x):
-	pass
-
+	return(x * 3953531 )
 def decode(y):
-	pass
+        return(y/3953531)
+
+f1=hello_world()
+
+	
+f2 =greet_by_name("Joyce")
+f3 = encode(2)
+f4 = decode(767)
+print(f1)
+print(f2)
+print(f3)
+print(f4)
+if( f1 == "hello world") :
+        print("passed")
+else:
+        print("failed")
+if (f2 == "hello" +"Joyce"):
+            print("passed")
+else:
+        print("failed")
+if (f3 == 2*3953531):
+            print("passed")
+else:
+        print("failed")
+
+        
+        
+
+
--- part2.py
+++ part2.py
@@ -1,7 +1,35 @@
 # Part 2 of the Python Review lab.
+def encode1(a):
+        if a > 1:
+                for i in range(2,a):
+                        if a%i ==0:
+                              return False
+                        
+                return True        
+                
 
 def encode(x, y):
-	pass
+        while not encode1(x):
+                x += 1
+        while not encode1(y):
+                y += 1
+        if 1< y <250 and 500<x<1000:
+                return (x*y)
+	
+
+	else:
+		print("Invalid input: Outside range")
+		return (None)
+f5 = encode(700,500)
+print(f5)	
 
 def decode(coded_message):
-	pass
+        for i in range (2,250):
+                if not encode(y):
+                        continue
+                if coded_message /y ==0:
+                        x = coded_message / y
+                        if (encoded(int(x)) and 500<x<1000:
+                            return(x,y)
+
+	
